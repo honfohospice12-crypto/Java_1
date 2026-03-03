6fb0c1337a290970d4ee5b4a980d501a6fc583e2
Hospice_HONFO <honfohospice12@gmail.com>
2026-03-03 17:36:51 +0100

    Add files via upload

---
+++ Affichage_0_à_10.java
@@ -0,0 +1,9 @@
+public class Affichage_0_à_10 {
+    public static void main(String[] args){
+        int i ;
+        for(i =1; i<=10; i++){
+            System.out.println(i);
+        }
+    }
+    
+}
